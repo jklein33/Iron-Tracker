bc5e0c9b51ff5f0a46f4ea43de1835fdd842b254
jklein33 <110059997+jklein33@users.noreply.github.com>
2022-08-04 15:39:04 -0500

    Update irontrackerapp.py

--- irontrackerapp.py
+++ irontrackerapp.py
@@ -3,3 +3,12 @@ import pandas as pd
 import snowflake.connector
 import configparser
 
+st.title('Welcome to the Iron Tracker')
+
+st.header('Insert Workouts')
+
+st.text('Enter Exercise details to database')
+exerciseType = st.text_input('Exercise Type')
+exerciseDesc = st.text_input('Exercise Description')
+difficultyLevel = st.text_input('Difficulty Level')
+muscleGroup = st.text_input('Muscle Group')
